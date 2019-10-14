928e8d5c455f3586b92e9da5629a80d1a405718d
Clay26 <5711623+Clay26@users.noreply.github.com>
2019-10-13 21:51:31 -0400

    Initial Commit

---
+++ War/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  War
+//
+//  Created by Clay Jackson on 10/13/19.
+//  Copyright © 2019 Clay Jackson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
